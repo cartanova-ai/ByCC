8fcfc9d21dc9687ef1a73b41b4727347ccf0522f
DongSeonYoo <inko51366@naver.com>
2026-07-06 15:54:37 +0900

    fix(qgrid/web): icon 타입 선언 로드

--- packages/web/src/vite-env.d.ts
+++ packages/web/src/vite-env.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="vite/client" />
+/// <reference types="unplugin-icons/types/react" />
 
 declare const __QGRID_CLI_VERSION__: string;
-/// <reference types="unplugin-icons/types/react" />
